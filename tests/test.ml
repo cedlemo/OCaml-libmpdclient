994b18fb107eb04f1ca42eddf8962522a8e423fc
cedlemo <cedlemo@gmx.com>
2016-12-21 11:37:11 +0100

    Make the test works

--- tests/test.ml
+++ tests/test.ml
@@ -2,20 +2,29 @@
 (* ocamlfind ocamlc -o test -package oUnit,str,libmpdclient -linkpkg -g test.ml *)
 
 open OUnit2
+open Mpd
+open Protocol
 
-let test_ok test_ctxt =  assert_equal Ok (LibMpd.parse_response "OK\n")
-let test_error_50 test_ctxt =  assert_equal true (let response = MpdResponses.parse_response "ACK [50@1] {play} error while playing\n" in
+let test_ok test_ctxt =  assert_equal true (let response = Protocol.parse_response "OK\n" in
+match response with
+| Ok -> true
+| Error _ -> false
+)
+
+let test_error_50 test_ctxt =  assert_equal true (let response = Protocol.parse_response "ACK [50@1] {play} error while playing\n" in
 match response with
 | Ok -> false
 | Error (er_val, cmd_num, cmd, message) -> er_val = No_exist && cmd_num = 1 && cmd = "play" &&
 message = "error while playing"
 )
-let test_error_1 test_ctxt =  assert_equal true (let response = Mpd.parse_response "ACK [1@12] {play} error while playing\n" in
+
+let test_error_1 test_ctxt =  assert_equal true (let response = Protocol.parse_response "ACK [1@12] {play} error while playing\n" in
 match response with
 | Ok -> false
 | Error (er_val, cmd_num, cmd, message) -> er_val = Not_list && cmd_num = 12 && cmd = "play" &&
 message = "error while playing"
 )
+
 let mpd_responses_parsing_tests =
   "Mpd responses parsing tests" >:::
     ["test OK" >:: test_ok;
