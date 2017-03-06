d0f3302cc32bc50661bef01f39194b6ff858efd1
cedlemo <cedlemo@gmx.com>
2017-03-06 21:43:29 +0100

    Add Mpd.Utils.values_of_pairs function

--- src/mpd_utils.ml
+++ src/mpd_utils.ml
@@ -10,6 +10,13 @@ let read_key_val str =
   let v =  List.hd (List.rev two_str_list) in
   {key = List.hd two_str_list; value = v}
 
+let values_of_pairs list_of_pairs =
+  let rec _values pairs acc =
+    match pairs with
+    | [] -> acc
+    | pair :: remainder -> let {key = _; value = v} = read_key_val pair in _values remainder (v :: acc)
+  in _values list_of_pairs []
+
 let bool_of_int_str b =
   match b with
   | "0" -> false
