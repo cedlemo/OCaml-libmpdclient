92e0f97f9b0a4fa0428d0b62ed07abc856b80f54
cedlemo <cedlemo@gmx.com>
2017-03-19 18:25:16 +0100

    Mpd.Song : extend the tags supported

--- src/mpd_song.ml
+++ src/mpd_song.ml
@@ -1,32 +1,52 @@
 open Mpd_utils
 type s = {
-  file: string;
-  last_modified:  string;
-  artist: string;
-  title: string;
   album: string;
-  track: int;
-  rate: int;
-  genre: string;
-  time: int;
+  albumsort: string; (* TODO : int ? *)
+  albumartist: string;
+  albumartistsort: string; (* TODO : int ? *)
+  artist: string;
+  artistsort: string; (* TODO : int ? *)
+  comment: string;
+  composer: string;
+  date: string;
+  disc: string; (* TODO : num_on_num *)
   duration: float;
-  pos: int;
+  file: string;
+  genre: string;
   id: int;
+  last_modified: string;
+  name: string;
+  performer: string;
+  pos: int;
+  rate: int;
+  time: int;
+  title: string;
+  track: string; (* TODO : num_on_num *)
 }
 
 let empty = {
-  file = "";
-  last_modified =  "";
-  artist = "";
-  title = "";
   album = "";
-  track = 0;
-  rate = 0;
-  genre = "";
-  time = 0;
+  albumsort = "";
+  albumartist = "";
+  albumartistsort = "";
+  artist = "";
+  artistsort = "";
+  comment = "";
+  composer = "";
+  date = "";
+  disc = "";
   duration = 0.0;
-  pos = 0;
+  file = "";
+  genre = "";
   id = 0;
+  last_modified =  "";
+  name = "";
+  performer = "";
+  pos = 0;
+  rate = 0;
+  time = 0;
+  title = "";
+  track = "";
 }
 
 let parse lines =
@@ -36,25 +56,59 @@ let parse lines =
     | p :: remain -> let { key = k; value = v} = Mpd_utils.read_key_val p in
     match k with
       | "album" -> _parse remain { s with album = v }
+      | "albumsort" -> _parse remain { s with albumsort = v }
+      | "albumartist" -> _parse remain { s with albumartist = v }
+      | "albumartistsort" -> _parse remain { s with albumartistsort = v }
       | "artist" -> _parse remain { s with artist = v }
+      | "artistsort" -> _parse remain { s with artistsort = v }
+      | "comment" -> _parse remain { s with comment = v }
+      | "composer" -> _parse remain { s with composer = v }
+      | "date" -> _parse remain { s with date = v }
+      | "disc" -> _parse remain { s with disc = v }
       | "duration" -> _parse remain { s with duration = float_of_string v }
       | "genre" -> _parse remain { s with album = v }
       | "file" -> _parse remain { s with file = v }
       | "id" -> _parse remain { s with id = int_of_string v }
       | "Last-Modified" -> _parse remain { s with last_modified = v }
+      | "name" -> _parse remain { s with title = v }
+      | "performer" -> _parse remain { s with performer = v }
       | "pos" -> _parse remain { s with pos = int_of_string v }
       | "rate" -> _parse remain { s with rate = int_of_string v }
       | "time" -> _parse remain { s with time = int_of_string v }
       | "title" -> _parse remain { s with title = v }
-      | "track" -> _parse remain { s with track = int_of_string v }
+      | "track" -> _parse remain { s with track = v }
       | _ -> _parse remain s
     in _parse lines empty
 
 let album {album = a; _} =
   a
 
+let albumsort {albumsort = a; _} =
+  a
+
+let albumartist {albumartist = a; _} =
+  a
+
+let albumartistsort {albumartist = a; _} =
+  a
+
 let artist {artist = a; _} =
   a
+
+let artistsort {artist = a; _} =
+  a
+
+let comment {comment = c; _} =
+  c
+
+let composer {composer = c; _} =
+  c
+
+let date {date = d; _} =
+  d
+
+let disc {disc = d; _} =
+  d
 
 let duration {duration = d; _} =
   d
@@ -70,6 +124,12 @@ let id {id = i; _} =
 
 let last_modified {last_modified = l; _} =
   l
+
+let name {name = n; _} =
+  n
+
+let performer {performer = p; _} =
+  p
 
 let pos {pos = p; _} =
   p
--- src/mpd_song.mli
+++ src/mpd_song.mli
@@ -2,14 +2,32 @@ type s
 val empty: s
 val parse: string list -> s
 val album: s -> string
+val albumsort: s -> string
+val albumartist: s -> string
+val albumartistsort: s -> string
 val artist: s -> string
+val artistsort: s -> string
+val comment: s -> string
+val composer: s -> string
+val date: s -> string
+val disc: s -> string
 val duration: s -> float
 val file: s -> string
 val genre: s -> string
 val id: s -> int
-val last_modified: s ->  string
+val last_modified: s -> string
+val name: s -> string
+val performer: s -> string
 val pos: s -> int
 val rate:  s -> int
 val time: s -> int
 val title: s -> string
-val track: s -> int
+val track: s -> string
+
+(*
+TAG_MUSICBRAINZ_ARTISTID
+TAG_MUSICBRAINZ_ALBUMID
+TAG_MUSICBRAINZ_ALBUMARTISTID
+TAG_MUSICBRAINZ_TRACKID
+TAG_MUSICBRAINZ_RELEASETRACKID
+TAG_NUM_OF_ITEM_TYPES  *)
