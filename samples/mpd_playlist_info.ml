9952be8f363f9e8df048ad431b3c073d4f2461fd
cedlemo <cedlemo@gmx.com>
2017-03-23 15:42:53 +0100

    Add an example that show the playlist_info

---
+++ samples/mpd_playlist_info.ml
@@ -0,0 +1,24 @@
+open Sys
+open Unix
+
+(* compile with
+ * ocamlfind ocamlc -o mpd_playlist_info -package str,unix -linkpkg -g mpd_responses.ml mpd.ml mpd_playlist_info.ml
+ * or
+ * ocamlfind ocamlc -o mpd_playlist_info -package str,unix,libmpdclient -linkpkg -g mpd_playlist_info.ml
+ *)
+let host = "127.0.0.1"
+let port = 6600
+
+let () =
+   let connection = Mpd.Connection.initialize host port in
+   let client = Mpd.Client.initialize connection in
+   match Mpd.CurrentPlaylist.playlist client with
+   | Mpd.CurrentPlaylist.PlaylistError message -> print_endline message
+   | Mpd.CurrentPlaylist.Playlist playlist -> let n = List.length playlist in
+   print_endline ("Number of songs : " ^ (string_of_int n));
+   List.iter (fun song ->
+     let id = string_of_int (Mpd.Song.id song) in
+     let title = Mpd.Song.title song in
+     let album = Mpd.Song.album song in
+     print_endline (String.concat " " ["\t*"; id; title; album])) playlist;
+   Mpd.Connection.close connection
