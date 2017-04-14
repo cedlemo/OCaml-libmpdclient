a94cd5554b0ab75ced75e2a9e3f13e062bc583f6
cedlemo <cedlemo@gmx.com>
2017-04-14 15:45:21 +0200

    Remove * char in multiline comments

--- src/mpdLwtQueue.ml
+++ src/mpdLwtQueue.ml
@@ -31,7 +31,7 @@ let clear client =
   Mpd.LwtClient.send client "clear"
 
 (** Deletes a song or a set of songs from the playlist. The song or the range
- * of songs are identified by the position in the playlist. *)
+    of songs are identified by the position in the playlist. *)
 let delete client position ?position_end () =
   let cmd = match position_end with
   |None -> String.concat " " ["delete"; string_of_int position]
@@ -46,7 +46,7 @@ let deleteid client id =
   Mpd.LwtClient.send client (String.concat " " ["deleteid"; string_of_int id])
 
 (** Moves the song at FROM or range of songs at START:END to TO in
- * the playlist. *)
+ the playlist. *)
 let move client position_from ?position_end position_to () =
   let cmd = match position_end with
   |None -> String.concat " " ["move";
@@ -61,8 +61,8 @@ let move client position_from ?position_end position_to () =
 in Mpd.LwtClient.send client cmd
 
 (** Moves the song with FROM (songid) to TO (playlist index) in the playlist.
- * If TO is negative, it is relative to the current song in the playlist
- * (if there is one). *)
+ If TO is negative, it is relative to the current song in the playlist
+ (if there is one). *)
 let moveid client id position_to =
   Mpd.LwtClient.send client (String.concat " " ["moveid";
                                                 string_of_int id;
