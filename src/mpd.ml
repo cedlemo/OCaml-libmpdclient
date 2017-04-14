f927c64b806822b490a2e77c271d90f00cbf55a8
cedlemo <cedlemo@gmx.com>
2017-04-14 18:53:15 +0200

    LwtConnection.read_command_response must return the full response for Protocol.parse_response

--- src/mpd.ml
+++ src/mpd.ml
@@ -258,23 +258,23 @@ end = struct
     | Incomplete
     | Complete of string
 
-  let check_full_response mpd_data pattern =
+  let check_full_response mpd_data pattern group =
     let response = Str.regexp pattern in
     match Str.string_match response mpd_data 0 with
-    | true -> Complete (Str.matched_group 1 mpd_data)
+    | true -> Complete (Str.matched_group group mpd_data)
     | false -> Incomplete
 
   let full_mpd_idle_event mpd_data =
     let pattern = "changed: \\(\\(\n\\|.\\)*\\)\nOK\n" in
-    check_full_response mpd_data pattern
+    check_full_response mpd_data pattern 1
 
   let full_mpd_banner mpd_data =
     let pattern = "OK\\(\\(\n\\|.\\)*\\)\n" in
-    check_full_response mpd_data pattern
+    check_full_response mpd_data pattern 1
 
   let full_mpd_command_response mpd_data =
     let pattern = "\\(\\(\n\\|.\\)*\\)OK\n" in
-    check_full_response mpd_data pattern
+    check_full_response mpd_data pattern 0
 
   let read connection check_full_data =
     let rec _read connection acc =
@@ -356,7 +356,7 @@ end = struct
     >>= fun () ->
       LwtConnection.read_command_response c
       >>= fun response ->
-        let parsed_response = Protocol.parse_response response in
+      let parsed_response = Protocol.parse_response response in
         Lwt.return parsed_response
 
   (** Create a status request and returns the status under a Mpd.Status.s Lwt.t
