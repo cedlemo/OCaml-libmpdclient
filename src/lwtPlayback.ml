4026e33a88fcbd34c1ba3e6ffa453e74545c7c6f
cedlemo <cedlemo@gmx.com>
2017-04-17 18:44:37 +0200

    Create LwtPlayback module

---
+++ src/lwtPlayback.ml
@@ -0,0 +1,52 @@
+(*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-libmpdclient.
+ *
+ * OCaml-libmpdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-libmpdclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+let next client =
+  Mpd.LwtClient.send client "next"
+
+let prev client =
+  Mpd.LwtClient.send client "prev"
+
+let stop client =
+  Mpd.LwtClient.send client "stop"
+
+let pause client arg =
+  match arg with
+  | true -> Mpd.LwtClient.send client "pause 1"
+  | _    -> Mpd.LwtClient.send client "pause 0"
+
+let play client songpos =
+  Mpd.LwtClient.send client (String.concat " " ["play";
+                                                string_of_int songpos])
+
+let playid client songid =
+  Mpd.LwtClient.send client (String.concat " " ["playid";
+                                                string_of_int songid])
+
+let seek client songpos time =
+  Mpd.LwtClient.send client (String.concat " " ["seek";
+                                                string_of_int songpos;
+                                                string_of_float time])
+
+let seekid client songid time =
+  Mpd.LwtClient.send client (String.concat " " ["seekid";
+                                                string_of_int songid;
+                                                string_of_float time])
+
+let seekcur client time =
+  Mpd.LwtClient.send client (String.concat " " ["seekcur"; string_of_float time])
---
+++ src/lwtPlayback.mli
@@ -0,0 +1,50 @@
+(*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-libmpdclient.
+ *
+ * OCaml-libmpdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-libmpdclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(** Controlling playback functions in Lwt thread.
+  https://www.musicpd.org/doc/protocol/playback_commands.html *)
+
+(** Plays next song in the playlist. *)
+val next: Mpd.LwtClient.c -> Protocol.response Lwt.t
+
+(** Plays previous song in the playlist. *)
+val prev: Mpd.LwtClient.c -> Protocol.response Lwt.t
+
+(** Stops playing.*)
+val stop: Mpd.LwtClient.c -> Protocol.response Lwt.t
+
+(** Toggles pause/resumers playing *)
+val pause: Mpd.LwtClient.c -> bool -> Protocol.response Lwt.t
+
+(** Begins playing the playlist at song number. *)
+val play: Mpd.LwtClient.c -> int -> Protocol.response Lwt.t
+
+(** Begins playing the playlist at song id. *)
+val playid: Mpd.LwtClient.c -> int -> Protocol.response Lwt.t
+
+(** Seeks to the position time of entry songpos in the playlist. *)
+val seek: Mpd.LwtClient.c -> int -> float -> Protocol.response Lwt.t
+
+(** Seeks to the position time of song id. *)
+val seekid: Mpd.LwtClient.c -> int -> float -> Protocol.response Lwt.t
+
+(** Seeks to the position time within the current song.
+ TODO : If prefixed by '+' or '-', then the time is relative to the current
+ playing position
+ *)
+val seekcur: Mpd.LwtClient.c -> float -> Protocol.response Lwt.t
