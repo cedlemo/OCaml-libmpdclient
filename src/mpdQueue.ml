00d4660e119636e75ef926fec1833f41102482c6
cedlemo <cedlemo@gmx.com>
2017-05-11 19:23:33 +0200

    Add MpdQueue.cleartagid

--- src/mpdQueue.ml
+++ src/mpdQueue.ml
@@ -162,3 +162,9 @@ let rangeid client id ?range () =
       String.concat " " [cmd; r]
   in
   Mpd.Client.send client request
+
+let cleartagid client id tag =
+  let request = String.concat " " ["cleartagid";
+                                   string_of_int id;
+                                   tag] in
+  Mpd.Client.send client request
--- src/mpdQueue.mli
+++ src/mpdQueue.mli
@@ -84,16 +84,13 @@ val swapid: Mpd.Client.c -> int -> int -> Protocol.response
 val rangeid:
   Mpd.Client.c -> int -> ?range:(float * float) -> unit -> Protocol.response
 
+val cleartagid:
+  Mpd.Client.c -> int -> string -> Protocol.response
+
 (*
   plchanges {VERSION} [START:END]
   plchangesposid {VERSION} [START:END]
-
-  [7]
   addtagid {SONGID} {TAG} {VALUE}
-
-  Adds a tag to the specified song. Editing song tags is only possible for
-  remote songs. This change is volatile: it may be overwritten by tags received
-  from the server, and the data is gone when the song gets removed from the queue.
 
   cleartagid {SONGID} [TAG]
 
