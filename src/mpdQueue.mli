61eb1a87851c568e7a4f6fd54fdf77d04a235f3e
cedlemo <cedlemo@gmx.com>
2017-07-16 18:25:49 +0200

    Fix MpdQueue documentation issues

--- src/mpdQueue.mli
+++ src/mpdQueue.mli
@@ -16,66 +16,86 @@
  * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
-(* https://www.musicpd.org/doc/protocol/queue.html *)
+(** Module for Mpd current playlist manipulation. *)
 
-(* info: unit -> Playlist.p *) (* return current playlist information command is "playlistinfo"*)
+(** Playlist type. *)
 type p =
   | PlaylistError of string
   | Playlist of Song.s list
 
-(** Adds the file URI to the playlist (directories add recursively). URI can also be a single file. *)
-val add: Mpd.Client.c -> string -> Protocol.response
-val addid: Mpd.Client.c -> string -> int -> int
+(** Adds the file URI to the playlist (directories add recursively). URI can
+    also be a single file. *)
+val add:
+  Mpd.Client.c -> string -> Protocol.response
+
+(** Adds a song to the playlist (non-recursive) and returns the song id.
+    URI is always a single file or URL. *)
+val addid:
+  Mpd.Client.c -> string -> int -> int
 
 (** Clears the current playlist. *)
-val clear: Mpd.Client.c -> Protocol.response
+val clear:
+  Mpd.Client.c -> Protocol.response
 
 (** Deletes a song or a set of songs from the playlist. The song or the range
     of songs are identified by the position in the playlist. *)
-val delete: Mpd.Client.c -> int -> ?position_end:int -> unit -> Protocol.response
+val delete:
+  Mpd.Client.c -> int -> ?position_end:int -> unit -> Protocol.response
 
 (** Deletes the song SONGID from the playlist. *)
-val deleteid: Mpd.Client.c -> int -> Protocol.response
+val deleteid:
+  Mpd.Client.c -> int -> Protocol.response
 
 (** Moves the song at FROM or range of songs at START:END to TO in
     the playlist. *)
-val move: Mpd.Client.c -> int -> ?position_end:int -> int -> unit -> Protocol.response
+val move:
+  Mpd.Client.c -> int -> ?position_end:int -> int -> unit -> Protocol.response
 
 (** Moves the song with FROM (songid) to TO (playlist index) in the playlist.
     If TO is negative, it is relative to the current song in the playlist
     (if there is one). *)
-val moveid: Mpd.Client.c -> int -> int -> Protocol.response
+val moveid:
+  Mpd.Client.c -> int -> int -> Protocol.response
 
 (** Get the songs in the playlist *)
-val playlist: Mpd.Client.c -> p
+val playlist:
+  Mpd.Client.c -> p
 
 (** Get information for one song *)
-val playlistid: Mpd.Client.c -> int -> p
+val playlistid:
+  Mpd.Client.c -> int -> p
 
 (** Finds songs in the current playlist with strict matching.*)
-val playlistfind: Mpd.Client.c -> string -> string -> p
+val playlistfind:
+  Mpd.Client.c -> string -> string -> p
 
 (** Searches case-insensitively for partial matches in the current playlist. *)
-val playlistsearch: Mpd.Client.c -> string -> string -> p
+val playlistsearch:
+  Mpd.Client.c -> string -> string -> p
 
 (** Swaps the positions of SONG1 and SONG2. *)
-val swap: Mpd.Client.c -> int -> int -> Protocol.response
+val swap:
+  Mpd.Client.c -> int -> int -> Protocol.response
 
 (** Shuffles the current playlist. START:END is optional and specifies a range
     of songs. *)
-val shuffle: Mpd.Client.c -> ?range:(int * int) -> unit -> Protocol.response
+val shuffle:
+  Mpd.Client.c -> ?range:(int * int) -> unit -> Protocol.response
 
 (** Set the priority of the specified songs. A higher priority means that it
     will be played first when "random" mode is enabled.
     A priority is an integer between 0 and 255. The default priority of new
     songs is 0. *)
-val prio: Mpd.Client.c -> int -> ?range:(int * int) -> unit -> Protocol.response
+val prio:
+  Mpd.Client.c -> int -> ?range:(int * int) -> unit -> Protocol.response
 
 (** Same as prio, but address the songs with their id. *)
-val prioid: Mpd.Client.c -> int -> int list -> Protocol.response
+val prioid:
+  Mpd.Client.c -> int -> int list -> Protocol.response
 
 (** Swaps the positions of SONG1 and SONG2 (both song ids). *)
-val swapid: Mpd.Client.c -> int -> int -> Protocol.response
+val swapid:
+  Mpd.Client.c -> int -> int -> Protocol.response
 
 (** Specifies the portion of the song that shall be played. START and END are
   offsets in seconds (fractional seconds allowed); both are optional. Omitting
