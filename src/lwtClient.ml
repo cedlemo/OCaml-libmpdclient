4c22f50b1dcad40d0c7fc86ef394789628f31b5c
cedlemo <cedlemo@gmx.com>
2017-07-19 15:36:40 +0200

    Add LwtClient module

---
+++ src/lwtClient.ml
@@ -0,0 +1,68 @@
+(*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-libmpdclient.
+ *
+ * OCaml-libmpdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-libmpdclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+type c = {connection : LwtConnection.c; mpd_banner : string }
+
+let initialize connection =
+  LwtConnection.read_mpd_banner connection
+  >>= fun message ->
+  Lwt.return {connection = connection; mpd_banner = message}
+
+let close client =
+let {connection = connection; _} = client in
+LwtConnection.close connection
+
+let mpd_banner {mpd_banner = banner; _ } =
+  banner
+
+let rec idle client on_event =
+  let {connection = connection; _} = client in
+  let cmd = "idle\n" in
+  LwtConnection.write connection cmd
+  >>= fun () ->
+    LwtConnection.read_idle_events connection
+    >>= fun response ->
+      on_event response
+      >>=fun stop ->
+        match stop with
+        | true -> Lwt.return ()
+        | false -> idle client on_event
+
+let send client cmd =
+  let {connection = c; _} = client in
+  LwtConnection.write c (cmd ^ "\n")
+  >>= fun () ->
+    LwtConnection.read_command_response c
+    >>= fun response ->
+    let parsed_response = Protocol.parse_response response in
+      Lwt.return parsed_response
+
+let status client =
+  send client "status"
+  >>= fun response ->
+    match response with
+    | Ok (lines) -> let status_pairs = Utils.split_lines lines in
+    let status = Status.parse status_pairs in Lwt.return status
+    | Error (ack, ack_cmd_num, cmd, error) -> let status = Status.generate_error error in
+    Lwt.return status
+
+let ping client =
+  send client "ping"
+
+let password client mdp =
+  send client (String.concat " " ["password"; mdp])
---
+++ src/lwtClient.mli
@@ -0,0 +1,65 @@
+(*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-libmpdclient.
+ *
+ * OCaml-libmpdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-libmpdclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(** Provides functions and type in order to communicate to the mpd server
+ with commands and requests in Lwt threads. *)
+
+open Lwt
+open LwtConnection
+open Status
+open Protocol
+
+type c
+
+(** Initialize the client with a connection. *)
+val initialize:
+  LwtConnection.c -> c Lwt.t
+
+(** Close the client *)
+val close:
+  c -> unit Lwt.t
+
+(** Return the mpd banner that the server send at the first connection of the
+    client. *)
+val mpd_banner:
+  c -> string
+
+(** Loop on mpd event with the "idle" command
+    the on_event function take the event response as argument and return
+    true to stop or false to continue the loop *)
+val idle:
+  c -> (string -> bool Lwt.t) -> unit Lwt.t
+
+(** Send to the mpd server a command. The response of the server is returned
+    under the form of a Protocol.response type. *)
+val send:
+  c -> string -> Protocol.response Lwt.t
+
+(** Create a status request and returns the status under a Mpd.Status.s Lwt.t
+    type.*)
+val status:
+  c -> Status.s Lwt.t
+
+(** Does nothing but return "OK". *)
+val ping:
+  c -> Protocol.response Lwt.t
+
+(** This is used for authentication with the server. PASSWORD is simply the
+    plaintext password. *)
+val password:
+  c -> string -> Protocol.response Lwt.t
