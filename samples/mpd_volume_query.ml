81eecb00651390d639d44eeb7744879536eedd4e
cedlemo <cedlemo@gmx.com>
2017-07-23 19:18:31 +0200

    Update to new api of Mpd

--- samples/mpd_volume_query.ml
+++ samples/mpd_volume_query.ml
@@ -23,6 +23,8 @@ open Unix
  * ocamlfind ocamlc -o mpd_volume_query -package str,unix -linkpkg -g mpd_responses.ml mpd.ml mpd_volume_query.ml
  * or
  * ocamlfind ocamlc -o mpd_volume_query -package str,unix,libmpdclient -linkpkg -g mpd_volume_query.ml
+ * or
+ * jbuilder build samples/mpd_volume_query.exe
  *)
 let host = "127.0.0.1"
 let port = 6600
@@ -32,6 +34,6 @@ let () =
    let client = Mpd.Client.initialize connection in
    print_endline ("banner: " ^ (Mpd.Client.mpd_banner client));
    let s = Mpd.Client.status client in
-   let vol = Status.volume s in
+   let vol = Mpd.Status.volume s in
    print_endline (string_of_int vol);
    Mpd.Connection.close connection;
