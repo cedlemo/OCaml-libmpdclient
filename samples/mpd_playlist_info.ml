f17aae759a3704c1ffb825ea5cb250656d93a2fa
cedlemo <cedlemo@gmx.com>
2017-07-23 19:15:01 +0200

    Update mpd_playlist_info.ml sample

--- samples/mpd_playlist_info.ml
+++ samples/mpd_playlist_info.ml
@@ -23,6 +23,7 @@ open Unix
  * ocamlfind ocamlc -o mpd_playlist_info -package str,unix -linkpkg -g mpd_responses.ml mpd.ml mpd_playlist_info.ml
  * or
  * ocamlfind ocamlc -o mpd_playlist_info -package str,unix,libmpdclient -linkpkg -g mpd_playlist_info.ml
+ * or jbuilder build mpd_playlist_info.exe
  *)
 let host = "127.0.0.1"
 let port = 6600
@@ -30,13 +31,13 @@ let port = 6600
 let () =
    let connection = Mpd.Connection.initialize host port in
    let client = Mpd.Client.initialize connection in
-   match MpdQueue.playlist client with
-   | MpdQueue.PlaylistError message -> print_endline message
-   | MpdQueue.Playlist playlist -> let n = List.length playlist in
+   match Mpd.Queue.playlist client with
+   | Mpd.Queue.PlaylistError message -> print_endline message
+   | Mpd.Queue.Playlist playlist -> let n = List.length playlist in
    print_endline ("Number of songs : " ^ (string_of_int n));
    List.iter (fun song ->
-     let id = string_of_int (Song.id song) in
-     let title = Song.title song in
-     let album = Song.album song in
+     let id = string_of_int (Mpd.Song.id song) in
+     let title = Mpd.Song.title song in
+     let album = Mpd.Song.album song in
      print_endline (String.concat " " ["\t*"; id; title; album])) playlist;
    Mpd.Connection.close connection
