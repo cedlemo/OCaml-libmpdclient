a97863c680d73609d18226a0ffa8f3caf2305b16
cedlemo <cedlemo@gmx.com>
2017-12-01 22:25:08 +0100

    Fix tests that did not use Protocol.response as returned value

--- tests/Connection_and_client.ml
+++ tests/Connection_and_client.ml
@@ -34,7 +34,12 @@ let test_connection_initialize test_ctxt =
 
 let test_client_send test_ctxt =
   let client = init_client () in
-  let _ = assert_equal ~printer:(fun x -> x) "OK\n" (Mpd.Client.send "ping" client) in
+  let _ = match Mpd.Client.send client "ping" with
+  | Error _ -> assert_equal ~printer:(fun _ -> "This should not has been reached") false true
+  | Ok response_opt -> match response_opt with
+    | None -> assert_equal ~printer:(fun _ -> "This should not has been reached") false true
+    | Some response -> let _ = assert_equal ~printer:(fun x -> x) "OK\n" ()
+  in
   Mpd.Client.close client
 
 let test_client_banner test_ctxt =
@@ -53,7 +58,12 @@ let test_client_status test_ctxt =
 
 let test_client_banner test_ctxt =
   let client = init_client () in
-  let _ = assert_equal ~printer:(fun x -> x) "OK\n" (Mpd.Client.ping client) in
+  let _ = match Mpd.Client.ping client with
+  | Error _ -> assert_equal ~printer:(fun _ -> "This should not has been reached") false true
+  | Ok response_opt -> match response_opt with
+    | None -> assert_equal ~printer:(fun _ -> "This should not has been reached") false true
+    | Some response -> let _ = assert_equal ~printer:(fun x -> x) "OK\n" ()
+  in
   Mpd.Client.close client
 
 let tests =
