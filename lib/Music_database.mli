12fb13729b8a139513a55fcc2122cf170ee9d61f
cedlemo <cedlemo@gmx.com>
2017-12-16 19:45:17 +0100

    Clean comments

--- lib/Music_database.mli
+++ lib/Music_database.mli
@@ -80,7 +80,6 @@ val search:
 val searchadd:
   Client.t -> (tags * string) list -> Protocol.response
 
-
 (*
 
 count {TAG} {NEEDLE} [...] [group] [GROUPTYPE]
@@ -94,59 +93,36 @@ count genre metal date 2016 group artist
 
 
 sort sorts the result by the specified tag. Without sort, the order is undefined. Only the first tag value will be used, if multiple of the same type exist. To sort by "Artist", "Album" or "AlbumArtist", you should specify "ArtistSort", "AlbumSort" or "AlbumArtistSort" instead. These will automatically fall back to the former if "*Sort" doesn't exist. "AlbumArtist" falls back to just "Artist".
-
 window can be used to query only a portion of the real response. The parameter is two zero-based record numbers; a start number and an end number.
 
-
 list {TYPE} [FILTERTYPE] [FILTERWHAT] [...] [group] [GROUPTYPE] [...]
-
 Lists unique tags values of the specified type. TYPE can be any tag supported by MPD or file.
-
 Additional arguments may specify a filter like the one in the find command.
-
 The group keyword may be used (repeatedly) to group the results by one or more tags. The following example lists all album names, grouped by their respective (album) artist:
-
 list album group albumartist
 
 listfiles [URI]
-
 Lists the contents of the directory URI, including files are not recognized by MPD. URI can be a path relative to the music directory or an URI understood by one of the storage plugins. The response contains at least one line for each directory entry with the prefix "file: " or "directory: ", and may be followed by file attributes such as "Last-Modified" and "size".
-
 For example, "smb://SERVER" returns a list of all shares on the given SMB/CIFS server; "nfs://servername/path" obtains a directory listing from the NFS server.
 
 lsinfo [URI]
-
 Lists the contents of the directory URI.
-
 When listing the root directory, this currently returns the list of stored playlists. This behavior is deprecated; use "listplaylists" instead.
-
 This command may be used to list metadata of remote files (e.g. URI beginning with "http://" or "smb://").
-
 Clients that are connected via UNIX domain socket may use this command to read the tags of an arbitrary local file (URI is an absolute path).
 
 readcomments [URI]
-
 Read "comments" (i.e. key-value pairs) from the file specified by "URI". This "URI" can be a path relative to the music directory or an absolute path.
-
 This command may be used to list metadata of remote files (e.g. URI beginning with "http://" or "smb://").
-
 The response consists of lines in the form "KEY: VALUE". Comments with suspicious characters (e.g. newlines) are ignored silently.
-
 The meaning of these depends on the codec, and not all decoder plugins support it. For example, on Ogg files, this lists the Vorbis comments.
-
 Searches for any song that contains WHAT in tag TYPE and adds them to current playlist.
-
 Parameters have the same meaning as for find, except that search is not case sensitive.
 
 searchaddpl {NAME} {TYPE} {WHAT} [...]
-
 Searches for any song that contains WHAT in tag TYPE and adds them to the playlist named NAME.
-
 If a playlist by that name doesn't exist it is created.
-
 Parameters have the same meaning as for find, except that search is not case sensitive.
-
-
 
  *)
 (** Updates the music database: find new files, remove deleted files, update
