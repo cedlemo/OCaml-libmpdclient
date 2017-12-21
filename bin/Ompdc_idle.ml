58fe140b3c817a72929351451c18f0a360b4e3b3
cedlemo <cedlemo@gmx.com>
2017-12-21 20:39:12 +0100

    Rewrite Ompdc_idle

--- bin/Ompdc_idle.ml
+++ bin/Ompdc_idle.ml
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with OCaml-libmpdclient.  If not, see <http://www.gnu.org/licenses/>.
  *)
+
 open Lwt.Infix
 open Notty
 open Notty_lwt
@@ -22,50 +23,76 @@ open Ompdc_common
 
 module Terminal = Notty_lwt.Term
 
+type status = {
+  timestamp : float;
+  state : Mpd.Status.state;
+  volume : int;
+  queue : Mpd.Queue_lwt.t;
+  song : int;
+}
+
+let fetch_status client =
+  Mpd.Client_lwt.status client
+  >>= fun response ->
+    match response with
+    | Error message -> Lwt.return (Error message)
+    | Ok s ->
+        let timestamp = Unix.time () in
+        let state = Mpd.Status.state s in
+        let volume = Mpd.Status.volume s in
+        let song = Mpd.Status.song s in
+        Mpd.Queue_lwt.playlist client
+        >>= fun queue ->
+          Lwt.return (Ok {timestamp; state; volume; queue; song})
+
+let update_status status client =
+  match status with
+  | Error _ -> Lwt.return status
+  | Ok s -> Mpd.Client_lwt.noidle client
+      >>= fun () ->
+        let now = Unix.time () in
+        if ((now -. s.timestamp) > 4.0) then fetch_status client
+        else Lwt.return status
+
 let gen_state_img status =
-  let state = Mpd.Status.state status in
-    let state_img = match state with
-      | Mpd.Status.Play -> I.(string A.(fg green) "play")
-      | Mpd.Status.Pause -> I.(string A.(fg lightblack) "Pause")
-      | Mpd.Status.Stop -> I.(string A.(fg black ++ bg lightblack) "Stop")
-      | Mpd.Status.ErrState -> I.(string A.(fg red) "State Error")
+  let state_img = match status.state with
+    | Mpd.Status.Play -> I.(string A.(fg green) "play")
+    | Mpd.Status.Pause -> I.(string A.(fg lightblack) "Pause")
+    | Mpd.Status.Stop -> I.(string A.(fg black ++ bg lightblack) "Stop")
+    | Mpd.Status.ErrState -> I.(string A.(fg red) "State Error")
   in
   I.(string A.(fg white) "[state ] : " <|> state_img)
 
 let gen_volume_img status =
-  let volume = Mpd.Status.volume status in
-  I.(strf ~attr:A.(fg white)   "[volume] : %d" volume)
+  I.(strf ~attr:A.(fg white)   "[volume] : %d" status.volume)
 
-let gen_playlist_img status client (w, h) =
-  let current_song = Mpd.Status.song status in
-  Mpd.Queue_lwt.playlist client
-  >>= function
-    | PlaylistError message -> Lwt.return I.(strf ~attr:A.(fg red) "Error: %s" message)
-    | Playlist songs ->
-      let gen_song_img i song =
-        let title = Mpd.Song.title song in
-        let artist = Mpd.Song.artist song in
-        if current_song = i then
-          I.(strf ~attr:A.(fg lightred ++ bg lightblack) "+ %s : %s" title artist)
-        else
-          I.(strf ~attr:A.(fg lightblack) "- %s : %s" title artist)
-      in
-      let song_imgs = List.mapi gen_song_img songs in
-      let lines = List.map (fun i ->
+let gen_playlist_img status (w, h) =
+  match status.queue with
+  | PlaylistError message -> Lwt.return I.(strf ~attr:A.(fg red) "Error: %s" message)
+  | Playlist songs ->
+    let gen_song_img i song =
+      let title = Mpd.Song.title song in
+      let artist = Mpd.Song.artist song in
+      if status.song = i then
+        I.(strf ~attr:A.(fg lightred ++ bg lightblack) "+ %s : %s" title artist)
+      else
+        I.(strf ~attr:A.(fg lightblack) "- %s : %s" title artist)
+    in
+    let song_imgs = List.mapi gen_song_img songs in
+    let lines = List.map (fun i ->
       let left_margin = 4 in
       let i_w = I.width i in
       let remain = let r = w - (i_w + left_margin) in (max r 0) in
-      I.hpad left_margin remain i) song_imgs in
-      Lwt.return I.(vcat lines)
+      I.hpad left_margin remain i)
+      song_imgs in
+    Lwt.return I.(vcat lines)
 
-let render (w, h) client =
-  Mpd.Client_lwt.status client
-  >>= fun response ->
-    match response with
+let render status (w, h) =
+    match status with
     | Error message -> Lwt.return I.(strf ~attr:A.(fg red) "[there is a pb %s]" message)
     | Ok status -> let state_img = gen_state_img status in
       let volume_img = gen_volume_img status in
-      gen_playlist_img status client (w, h)
+      gen_playlist_img status (w, h)
       >>= fun songs_img ->
       Lwt.return I.(state_img <-> volume_img <-> songs_img)
 
@@ -76,46 +103,45 @@ let event term = Lwt_stream.get (Terminal.events term) >|= function
   | Some (`Resize _ | #Unescape.event as x) -> x
   | None -> `End
 
-let rec loop term (e, t) dim client timestamp =
+let rec loop term (e, t) dim client status =
   (e <?> t) >>= function
   | `End | `Key (`Escape, []) | `Key (`ASCII 'C', [`Ctrl]) ->
-      Lwt.return_unit
+      Mpd.Client_lwt.close client
   | `Mpd_event event_name ->
-      render dim client
-      >>= fun img ->
-        Terminal.image term img
-        >>= fun () ->
-            let now = Unix.time () in
-            loop term (e, listen_mpd_event client) dim client now
-  | `Resize dim ->
-      let now = Unix.time () in
-      if (timestamp -. now >= 1.0) then
-        render dim client
+      fetch_status client
+      >>= fun status' ->
+        render status' dim
         >>= fun img ->
           Terminal.image term img
           >>= fun () ->
-          loop term (event term, t) dim client now
-      else loop term (event term, t) dim client timestamp
+            loop term (e, listen_mpd_event client) dim client status'
+  | `Resize dim ->
+      update_status status client
+      >>= fun status' ->
+        render status' dim
+        >>= fun img ->
+          Terminal.image term img
+          >>= fun () ->
+            loop term (event term, t) dim client status'
   | _ ->
-      let now = Unix.time () in
-      if (timestamp -. now >= 1.0) then
-      render dim client
-      >>= fun img ->
-        Terminal.image term img
-        >>= fun () ->
-        loop term (event term, t) dim client now
-      else loop term (event term, t) dim client timestamp
-
+      update_status status client
+      >>= fun status' ->
+        render status' dim
+        >>= fun img ->
+          Terminal.image term img
+          >>= fun () ->
+            loop term (event term, t) dim client status'
 
 let interface client =
   let term = Terminal.create () in
   let size = Terminal.size term in
-  render size client
-  >>= fun img ->
-    Terminal.image term img
-    >>= fun () ->
-    let timestamp = Unix.time () in
-    loop term (event term, listen_mpd_event client) size client timestamp
+  fetch_status client
+  >>= fun result_status ->
+    render result_status size
+    >>= fun img ->
+      Terminal.image term img
+      >>= fun () ->
+        loop term (event term, listen_mpd_event client) size client result_status
 
 let idle common_opts =
   let open Mpd in
