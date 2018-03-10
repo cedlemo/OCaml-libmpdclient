9d940a07870e66716cd579e57125dfdc784b4711
cedlemo <cedlemo@gmx.com>
2018-03-10 22:30:57 +0100

    add test for stop command

--- tests/Test_playback_lwt.ml
+++ tests/Test_playback_lwt.ml
@@ -31,11 +31,56 @@ let init_client () =
 let run_test f =
   ignore(Lwt_main.run begin
     init_client ()
-    >>= client ->
+    >>= fun client ->
       f client
-      >>= () ->
+      >>= fun () ->
         Mpd.Client_lwt.close client
   end)
 
+let test_play_pause_stop test_ctxt =
+  run_test begin fun client ->
+    let queue_length () =
+      Mpd.Queue_lwt.playlist client
+      >|= function
+        | Mpd.Queue_lwt.PlaylistError _ -> -1
+        | Mpd.Queue_lwt.Playlist p -> List.length p
+    in
+    let check_state s test_name =
+      Mpd.Client_lwt.status client
+      >|= fun status ->
+        match status with
+        | Error message ->
+            assert_equal ~printer:(fun s -> test_name ^ s)
+                         "Unable to get status" message
+        | Ok status ->
+            assert_equal ~printer:(fun s ->
+              test_name ^ (Mpd.Status.string_of_state s)
+            ) s (Mpd.Status.state status)
+    in
+    let delayed_check_state s test_name =
+      Lwt_unix.sleep 2.0
+      >>= fun () ->
+        check_state s test_name
+    in
+    queue_length ()
+    >>= fun l ->
+      if l <= 0 then begin
+        Mpd.Stored_playlists_lwt.load client "bach" ()
+        >>= function
+          | Error (_, _, _, message) ->
+              let information = "Error when loading playlist" in
+              let _ = assert_equal ~printer:(fun s -> s) information message in
+              Lwt.return_unit
+          | Ok _ ->
+              Lwt.return_unit
+      end
+      else Lwt.return_unit
+      >>= fun () ->
+      check_state Mpd.Status.Stop "Initial state "
+  end
+
 let tests =
-  "Playback and Playback_options tests" >::: []
+  "Playback and Playback_options tests" >:::
+    [
+      "Test play pause stop" >:: test_play_pause_stop
+    ]
