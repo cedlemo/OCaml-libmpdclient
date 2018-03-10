65c5c61240f49123e3933ffe3ca2e142f5a3bd85
cedlemo <cedlemo@gmx.com>
2018-03-10 18:23:14 +0100

    Test_playback: fix style issues

--- tests/Test_playback.ml
+++ tests/Test_playback.ml
@@ -39,40 +39,60 @@ let test_play_pause_stop test_ctxt =
     in
     let check_state s test_name =
       match Mpd.Client.status client with
-      | Error message -> assert_equal ~printer:(fun s -> test_name ^ s) "Unable to get status" message
+      | Error message ->
+          assert_equal ~printer:(fun s -> test_name ^ s)
+                       "Unable to get status" message
       | Ok status ->
-          assert_equal ~printer:(fun s -> test_name ^ (Mpd.Status.string_of_state s))
-                       s (Mpd.Status.state status)
+          assert_equal ~printer:(fun s ->
+            test_name ^ (Mpd.Status.string_of_state s)
+          ) s (Mpd.Status.state status)
+    in
+    let check_state_w_delay s test_name =
+      let _ = Unix.sleep 2 in
+      check_state s test_name
     in
     if queue_length () <= 0 then (
       match Mpd.Stored_playlists.load client "bach" () with
-      | Error (_, _, _, message) -> assert_equal ~printer:(fun s -> s) "Error when loading playlist" message
-      | Ok _ -> ()
+      | Error (_, _, _, message) ->
+          let information = "Error when loading playlist" in
+          assert_equal ~printer:(fun s -> s)  information message
+      | Ok _ ->
+          ()
     );
     let _ = check_state Mpd.Status.Stop "Initial state " in
     let _ = (
       match Mpd.Playback.pause client false with
-      | Error (_, _ , _, message) -> assert_equal ~printer:(fun s -> s) "Unable to disable pause " message
-      | Ok _ -> let _ = Unix.sleep 2 in check_state Mpd.Status.Stop "Pause command false before play"
+      | Error (_, _ , _, message) ->
+          assert_equal ~printer:(fun s -> s) "Unable to disable pause " message
+      | Ok _ ->
+          check_state_w_delay Mpd.Status.Stop "Pause command false before play"
     ) in
     let _ = (
       match Mpd.Playback.play client 1 with
-      | Error (_, _ , _, message) -> assert_equal ~printer:(fun s -> s) "Unable to play " message
-      | Ok _ -> let _ = Unix.sleep 2 in check_state Mpd.Status.Play "Play command "
+      | Error (_, _ , _, message) ->
+          assert_equal ~printer:(fun s -> s) "Unable to play " message
+      | Ok _ ->
+          check_state_w_delay Mpd.Status.Play "Play command "
     ) in
     let _ = (
       match Mpd.Playback.pause client true with
-      | Error (_, _ , _, message) -> assert_equal ~printer:(fun s -> s) "Unable to pause " message
-      | Ok _ -> let _ = Unix.sleep 2 in check_state Mpd.Status.Pause "Pause command true "
+      | Error (_, _ , _, message) ->
+          assert_equal ~printer:(fun s -> s) "Unable to pause " message
+      | Ok _ ->
+          check_state_w_delay Mpd.Status.Pause "Pause command true "
     ) in
     let _ = (
       match Mpd.Playback.pause client false with
-      | Error (_, _ , _, message) -> assert_equal ~printer:(fun s -> s) "Unable to replay " message
-      | Ok _ -> let _ = Unix.sleep 2 in check_state Mpd.Status.Play "Pause command false "
+      | Error (_, _ , _, message) ->
+          assert_equal ~printer:(fun s -> s) "Unable to replay " message
+      | Ok _ ->
+          check_state_w_delay Mpd.Status.Play "Pause command false "
     ) in
     match Mpd.Playback.stop client with
-    | Error (_, _ , _, message) -> assert_equal ~printer:(fun s -> s) "Unable to stop " message
-    | Ok _ -> let _ = Unix.sleep 2 in check_state Mpd.Status.Stop "Stop command at end"
+    | Error (_, _ , _, message) ->
+        assert_equal ~printer:(fun s -> s) "Unable to stop " message
+    | Ok _ ->
+        check_state_w_delay Mpd.Status.Stop "Stop command at end"
   )
 
 let tests =
