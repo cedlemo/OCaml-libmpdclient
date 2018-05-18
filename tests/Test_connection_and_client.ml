126dd0e9b77359e325eb7001237eb5a9f1e71410
cedlemo <cedlemo@gmx.com>
2018-05-18 22:04:34 +0200

    Fix style issues

--- tests/Test_connection_and_client.ml
+++ tests/Test_connection_and_client.ml
@@ -20,68 +20,78 @@ open OUnit2
 open Mpd
 open Test_configuration
 
+module Clt = Mpd.Client
+module Cnx = Mpd.Connection
+
+let msg = "This should not has been reached"
+
 let init_client () =
-  let connection = Mpd.Connection.initialize host port in
-  Mpd.Client.initialize connection
+  let connection = Cnx.initialize host port in
+  Clt.initialize connection
 
 let test_connection_initialize test_ctxt =
-  let connection = Mpd.Connection.initialize host port in
-  let _ = assert_equal ~printer:(fun s -> s) host (Connection.hostname connection) in
-  let _ = assert_equal ~printer:string_of_int port (Connection.port connection) in
-  Mpd.Connection.close connection
+  let connection = Cnx.initialize host port in
+  let _ = assert_equal ~printer:(fun s -> s) host (Cnx.hostname connection) in
+  let _ = assert_equal ~printer:string_of_int port (Cnx.port connection) in
+  Cnx.close connection
 
 let test_client_send test_ctxt =
   let client = init_client () in
   let _ = (
-    match Mpd.Client.send client "ping" with
-    | Error _ -> assert_equal ~msg:"This should not has been reached" false true
+    match Clt.send client "ping" with
+    | Error _ -> assert_equal ~msg false true
     | Ok response_opt -> match response_opt with
       | None -> assert_equal true true
-      | Some response -> assert_equal ~msg:"This should not has been reached" false true
+      | Some response -> assert_equal ~msg false true
   )
   in
-  Mpd.Client.close client
+  Clt.close client
 
 let test_client_banner test_ctxt =
   let client = init_client () in
   let pattern = "MPD [0-9].[0-9][0-9].[0-9]" in
-  let banner = Mpd.Client.mpd_banner client in
+  let banner = Clt.mpd_banner client in
   let msg = Printf.sprintf "Banner : %s" banner in
-  let _ = assert_equal true ~msg Str.(string_match (regexp pattern) banner 0) in
-  Mpd.Client.close client
+  let _ =
+    assert_equal true ~msg Str.(string_match (regexp pattern) banner 0) in
+  Clt.close client
 
 let test_client_status test_ctxt =
   let client = init_client () in
   let _ = match Client.status client with
-    | Error message -> assert_equal ~printer:(fun _ -> "This should not have been reached") true false
-    | Ok status -> let state = Mpd.(Status.string_of_state (Status.state status)) in
+    | Error message ->
+      assert_equal ~printer:(fun _ -> msg) true false
+    | Ok status ->
+      let state = Mpd.(Status.string_of_state (Status.state status)) in
       assert_equal ~printer:(fun s -> s) "stop" state
   in
-  Mpd.Client.close client
+  Clt.close client
 
 let test_client_ping test_ctxt =
   let client = init_client () in
-  let _ = match Mpd.Client.ping client with
-  | Error _ -> assert_equal ~msg:"This should not has been reached" false true
-  | Ok response_opt -> match response_opt with
-    | None -> assert_equal true true
-    | Some response -> assert_equal ~msg:"This should not has been reached" false true
+  let _ = match Clt.ping client with
+    | Error _ -> assert_equal ~msg false true
+    | Ok response_opt ->
+      match response_opt with
+      | None -> assert_equal true true
+      | Some response -> assert_equal ~msg false true
   in
-  Mpd.Client.close client
+  Clt.close client
 
 let test_client_tagtypes test_ctxt =
   let client = init_client () in
-  let tagtypes = Mpd.Client.tagtypes client in
-  let _ = assert_equal ~printer:string_of_bool true (List.length tagtypes > 0) in
+  let tagtypes = Clt.tagtypes client in
+  let _ =
+    assert_equal ~printer:string_of_bool true (List.length tagtypes > 0) in
   assert_equal ~printer:string_of_bool true (List.mem "Artist" tagtypes)
 
 let tests =
   "Connection and client tests" >:::
-    [
-      "Connection initialize test" >:: test_connection_initialize;
-      "Client send test" >:: test_client_send;
-      "Client banner test" >:: test_client_banner;
-      "Client status test" >:: test_client_status;
-      "Client ping test" >:: test_client_ping;
-      "Client tagtypes" >:: test_client_tagtypes
-    ]
+  [
+    "Connection initialize test" >:: test_connection_initialize;
+    "Client send test" >:: test_client_send;
+    "Client banner test" >:: test_client_banner;
+    "Client status test" >:: test_client_status;
+    "Client ping test" >:: test_client_ping;
+    "Client tagtypes" >:: test_client_tagtypes
+  ]
