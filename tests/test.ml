24e9369c6c5e73fcfc33d7a1ef4191e12cd3251b
cedlemo <cedlemo@gmx.com>
2018-05-18 21:54:48 +0200

    Activate connection, client and regexes tests

--- tests/test.ml
+++ tests/test.ml
@@ -23,12 +23,12 @@ let () =
   (
     "Mpd client library tests" >:::
       [
-        Test_connection_and_client.tests;
-        Test_queue_and_playlists.tests;
-        (*
-        Test_playback.tests;
         Test_regexp_uses.tests;
+        Test_connection_and_client.tests;
         Test_connection_and_client_lwt.tests;
+        (*
+        Test_queue_and_playlists.tests;
+        Test_playback.tests;
         Test_playback_lwt.tests; *)
       ]
 )
