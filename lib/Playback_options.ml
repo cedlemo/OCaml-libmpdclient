4f7dea1f31d87c29d993f68f437e8edc1866e024
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-05 13:18:14 +0200

    Update copyright

--- lib/Playback_options.ml
+++ lib/Playback_options.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * Copyright 2017-2018 Cedric LE MOIGNE, cedlemo@gmx.com
  * This file is part of OCaml-libmpdclient.
  *
  * OCaml-libmpdclient is free software: you can redistribute it and/or modify
