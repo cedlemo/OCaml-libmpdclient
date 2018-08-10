013777dc254df0c9c826a7d89627b066b7aa633f
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-10 18:51:10 +0200

    Activate only regex tests

--- tests/test.ml
+++ tests/test.ml
@@ -24,9 +24,9 @@ let () =
     "Mpd client library tests" >:::
       [
         Test_regexp_uses.tests;
+        (*
         Test_connection_and_client.tests;
         Test_playback.tests;
-        (*
         Test_queue_and_playlists.tests;
         Test_connection_and_client_lwt.tests;
         Test_playback_lwt.tests; *)
