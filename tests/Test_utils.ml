044f7f6b4f76aa0f6db54af4688cc151754789df
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-25 18:15:37 +0200

    Add run_test_lwt in Test_utils

--- tests/Test_utils.ml
+++ tests/Test_utils.ml
@@ -1,4 +1,6 @@
 open OUnit
+open Lwt.Infix
+
 open Test_configuration
 
 let printer = fun s -> s
@@ -8,11 +10,16 @@ let init_client () =
   let client = Mpd.Client.initialize connection in
   let () = match Mpd.Music_database.update client None with
     | Error (_, _, _, message) ->
-        let information = "Error when updating database " in
-        assert_equal ~printer information message
+      let information = "Error when updating database " in
+      assert_equal ~printer information message
     | Ok _ -> ()
   in
   client
+
+let init_client_lwt () =
+  Mpd.Connection_lwt.initialize host port
+  >>= fun connection ->
+  Mpd.Client_lwt.initialize connection
 
 let queue_length client =
   match Mpd.Queue.playlist client with
@@ -23,13 +30,13 @@ let ensure_playlist_is_loaded client =
   if queue_length client <= 0 then begin
     match Mpd.Stored_playlists.load client "bach" () with
     | Error (_, _, _, message) ->
-        let information = "Error when loading playlist" in
-        assert_equal ~printer information message
+      let information = "Error when loading playlist" in
+      assert_equal ~printer information message
     | Ok _ -> ()
   end
 
 let ensure_playback_is_stopped client =
-   ignore(Mpd.Playback.stop client)
+  ignore(Mpd.Playback.stop client)
 
 let ensure_playlist_is_cleared client =
   ignore(Mpd.Queue.clear client)
@@ -38,6 +45,15 @@ let run_test f =
   let client = init_client () in
   let () = f client in
   Mpd.Client.close client
+
+let run_test_lwt f =
+ ignore(Lwt_main.run begin
+      init_client_lwt ()
+      >>= fun client ->
+      f client
+      >>= fun () ->
+      Mpd.Client_lwt.close client
+    end)
 
 let run_test_on_playlist f =
   run_test begin fun client ->
@@ -48,10 +64,10 @@ let run_test_on_playlist f =
 let assert_state client s test_name =
   match Mpd.Client.status client with
   | Error message ->
-      assert_equal ~printer:(fun s -> test_name ^ s)
-                   "Unable to get status" message
+    assert_equal ~printer:(fun s -> test_name ^ s)
+      "Unable to get status" message
   | Ok status ->
-      assert_equal ~printer:(fun s ->
+    assert_equal ~printer:(fun s ->
         test_name ^ (Mpd.Status.string_of_state s)
       ) s (Mpd.Status.state status)
 
@@ -62,6 +78,6 @@ let assert_state_w_delay _client s test_name =
 let check_state client s =
   match Mpd.Client.status client with
   | Error _message ->
-      false
+    false
   | Ok status ->
-       s == Mpd.Status.state status
+    s == Mpd.Status.state status
