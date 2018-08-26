f39298be3dfce838e8a40a8e61a4dcaf683dfd34
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-26 10:02:08 +0200

    Test load playlist lwt

--- tests/Test_queue_and_playlists_lwt.ml
+++ tests/Test_queue_and_playlists_lwt.ml
@@ -22,27 +22,53 @@ module TU = Test_utils
 
 let printer = TU.printer
 let queue_length = TU.queue_length
+let queue_length_lwt = TU.queue_length_lwt
 
 let test_stored_playlists_listplaylists _test_ctxt =
   TU.run_test_on_playlist_lwt begin fun client ->
     Mpd.Stored_playlists_lwt.listplaylists client
     >>= fun response ->
     let () = match response with
-    | Error message ->
-      assert_equal ~printer "This should not have been reached" message
-    | Ok playlists ->
-      let () = assert_equal ~printer:string_of_int 2 (List.length playlists) in
-      let hd = List.hd playlists in
-      let tail = List.hd (List.tl playlists) in
-      let test = "bach" = hd || "bach1" = hd in
-      let () = assert_bool "First playlist" test in
-      let test' = "bach" = tail || "bach1" = tail in
-      assert_bool "Last playlist" test'
+      | Error message ->
+        assert_equal ~printer "This should not have been reached" message
+      | Ok playlists ->
+        let () = assert_equal ~printer:string_of_int 2 (List.length playlists) in
+        let hd = List.hd playlists in
+        let tail = List.hd (List.tl playlists) in
+        let test = "bach" = hd || "bach1" = hd in
+        let () = assert_bool "First playlist" test in
+        let test' = "bach" = tail || "bach1" = tail in
+        assert_bool "Last playlist" test'
     in Lwt.return_unit
+  end
+
+let test_stored_playlists_load_playlist_and_clear _test_ctxt =
+  TU.run_test_lwt begin fun client ->
+    Mpd.Stored_playlists_lwt.load client "bach" ()
+    >>= function
+    | Error (_, _, _, message) ->
+      assert_equal ~printer "This should not have been reached " message;
+      Lwt.return_unit
+    | Ok _ ->
+      queue_length_lwt client
+      >>= fun len ->
+      let () =  assert_equal ~printer:string_of_int 11 len in
+      Mpd.Queue_lwt.clear client
+      >>= function
+      | Error (_, _, _, message) ->
+        assert_equal ~printer "This should not have been reached " message;
+        Lwt.return_unit
+      | Ok _ -> begin
+          queue_length_lwt client
+          >>= fun len ->
+          assert_equal ~printer:string_of_int 0 len;
+          Lwt.return_unit
+        end
   end
 
 let tests =
   "Queue and playlists lwt tests" >:::
   [
     "test stored playlists listplaylists" >:: test_stored_playlists_listplaylists;
+    "test stored playlists load playlist and clear" >:: test_stored_playlists_load_playlist_and_clear;
   ]
