7e3b6f9dec05e3d0b1da1d7311789ec43c428b91
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-30 19:51:08 +0200

    Fix style issue and clean code

--- lib/Music_database.ml
+++ lib/Music_database.ml
@@ -66,6 +66,9 @@ let tag_to_string = function
   | Album_sort -> "album_sort"
   | Count -> "count"
 
+(* TODO: check for warning :
+   constraints are not satisfied in this type. Type tags should be an instance of
+*)
 type search_tags = Any | File | Base | Modified_since | Mpd_tag of tags
 
 let search_tag_to_string = function
@@ -75,37 +78,43 @@ let search_tag_to_string = function
   | Modified_since -> "modified-since"
   | Mpd_tag t -> tag_to_string t
 
-let search_find_wrapper cmd_name client what_list ?sort:sort_tag ?window:window () =
-  let what =
-    List.map (fun (tag, param) -> Printf.sprintf "%s \"%s\"" (search_tag_to_string tag) param) what_list
-    |> String.concat " "
+let build_tag_parameter printer tags =
+  let tag_and_param_to_string (t, p) =
+    Printf.sprintf "%s \"%s\"" (printer t) p
   in
-  let sort = match sort_tag with
+  List.map tag_and_param_to_string tags
+  |> String.concat " "
+
+let search_find_wrapper cmd_name client what_list ?sort ?window () =
+  let what = build_tag_parameter search_tag_to_string what_list in
+  let sort = match sort with
     | None -> ""
     | Some tag -> " sort " ^ (tag_to_string tag)
   in
   let window = match window with
     | None -> ""
-    | Some (start, stop) -> Printf.sprintf " window %s:%s" (string_of_int start) (string_of_int stop)
+    | Some (start, stop) ->
+      Printf.sprintf " window %d:%d" start stop
   in
   let cmd = Printf.sprintf "%s %s%s%s" cmd_name what sort window in
   match Client.send_request client cmd with
   | Error err -> Error err
   | Ok response -> match response with
-      | None -> Ok []
-      | Some r -> let songs = Str.split (Str.regexp_string "file:") r
-        |> List.map (fun s -> Str.split (Str.regexp_string "\n") s |> Song.parse)
-        in Ok songs
+    | None -> Ok []
+    | Some r ->
+      let songs =
+        Str.split (Str.regexp_string "file:") r
+        |> let build_song s =
+             Utils.split_lines s |> Song.parse
+        in List.map build_song
+      in Ok songs
 
 let find = search_find_wrapper "find"
 
 let search = search_find_wrapper "search"
 
 let search_find_add_wrapper cmd_name client what_list =
-  let what =
-    List.map (fun (tag, param) -> Printf.sprintf "%s \"%s\"" (search_tag_to_string tag) param) what_list
-    |> String.concat " "
-  in
+  let what = build_tag_parameter search_tag_to_string what_list in
   let cmd = Printf.sprintf "%s %s" cmd_name what in
   Client.send_command client cmd
 
@@ -118,12 +127,9 @@ let searchaddpl client playlist_name what_list =
 
 type song_count = { songs: int; playtime: float; misc: string }
 
-let count client what_list ?group:group_tag () =
-  let what =
-    List.map (fun (tag, param) -> Printf.sprintf "%s \"%s\"" (tag_to_string tag) param) what_list
-    |> String.concat " "
-  in
-  let group = match group_tag with
+let count client what_list ?group () =
+  let what = build_tag_parameter tag_to_string what_list in
+  let group = match group with
     | None -> None
     | Some tag -> Some (tag_to_string tag)
   in
@@ -131,36 +137,36 @@ let count client what_list ?group:group_tag () =
   match Client.send_request client cmd with
   | Error (_, _, _, message) -> Error message
   | Ok response -> match response with
-      | None -> Ok []
-      | Some r ->
-          let result = Utils.parse_count_response r group in
-          let song_counts = List.map (fun (songs, playtime, misc) -> {songs; playtime; misc}) result in
-          Ok song_counts
+    | None -> Ok []
+    | Some r ->
+      let result = Utils.parse_count_response r group in
+      let tuple_to_song_count (songs, playtime, misc) = {songs; playtime; misc}
+      in
+      let song_counts = List.map tuple_to_song_count result in
+      Ok song_counts
 
 let list client tag tag_list =
   let filter = tag_to_string tag |> String.capitalize_ascii in
-  let tags = List.map (fun (t, p) ->
-                       Printf.sprintf "%s \"%s\"" (tag_to_string t) p) tag_list
-            |> String.concat " " in
+  let tags = build_tag_parameter tag_to_string tag_list in
   let cmd = Printf.sprintf "list %s %s" filter tags in
   match Client.send_request client cmd with
   | Error (_, _, _, message) -> Error message
   | Ok response -> match response with
-      | None -> Ok []
-      | Some r -> let split_pattern = Printf.sprintf "\\(\n\\)*%s: " filter in
-        let l = match Str.split (Str.regexp split_pattern) r with
-          | [] -> []
-          | h :: t -> if h = "" then t else (h :: t)
-        in Ok l
+    | None -> Ok []
+    | Some r -> let split_pattern = Printf.sprintf "\\(\n\\)*%s: " filter in
+      let l = match Str.split (Str.regexp split_pattern) r with
+        | [] -> []
+        | h :: t -> if h = "" then t else (h :: t)
+      in Ok l
 
 let update client uri =
   let cmd = match uri with
-  | None -> "update"
-  | Some uri' -> "update " ^ uri'
+    | None -> "update"
+    | Some uri' -> "update " ^ uri'
   in Client.send_request client cmd
 
 let rescan client uri =
   let cmd = match uri with
-  | None -> "rescan"
-  | Some uri' -> "rescan " ^ uri'
+    | None -> "rescan"
+    | Some uri' -> "rescan " ^ uri'
   in Client.send_command client cmd
